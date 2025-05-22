8c2843010d7730a483d79973617cc488bb14fd93
dzagame <152071876+dzagame@users.noreply.github.com>
2025-05-23 01:58:00 +0200

    Add files via upload

---
+++ src/BQ_1989_Replication.jl
@@ -0,0 +1,26 @@
+module BQ_1989_Replication
+
+using XLSX
+using DataFrames
+using Dates
+using LinearAlgebra
+using Distributions
+using Plots
+using Statistics
+using Pkg
+using Test
+
+
+
+export run
+
+#Import the main functions 
+function run()
+    include("src/filepath.jl")
+    include("src/IRFs.jl") #Generates the IRFs (figures 1-2-3-4-5-6)
+    #include("src/VarianceDecomp.jl") #Generates the Variance Decomposition tables
+end
+
+
+
+end # module 
