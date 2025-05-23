9d22d2432bbea2eee84d87c3ffb3e7e55948cb99
dzagame <152071876+dzagame@users.noreply.github.com>
2025-05-23 02:04:04 +0200

    Update files_path.jl

---
+++ src/files_path.jl
@@ -0,0 +1,10 @@
+# This document determines the file paths for the data and output files.
+
+#Path to the data file
+
+const input_raw_data = "data"
+
+#Path to the output files
+
+const output_figures ="output/figures"
+const output_tables = "output/tables"
