aec00175b7d4dc33c155f9cdbdbc1378b7e9f049
dzagame <152071876+dzagame@users.noreply.github.com>
2025-05-23 17:44:57 +0200

    Update BQ_1989_Replication.jl

--- src/BQ_1989_Replication.jl
+++ src/BQ_1989_Replication.jl
@@ -9,16 +9,21 @@ using Plots
 using Statistics
 using Pkg
 using Test
+using TestItemRunner
 
 
 
-export run
+export run, runtests
 
 #Import the main functions 
 function run()
     include("src/files_path.jl")
     include("src/IRFs.jl") #Generates the IRFs (figures 1-2-3-4-5-6)
     #include("src/VarianceDecomp.jl") #Generates the Variance Decomposition tables
+end
+
+function runtests()
+    include("test/runtests.jl")
 end
 
 
